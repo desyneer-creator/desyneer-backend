0f54a324afe5480cbb1065520c21c4047b375977
desyneer-creator <fatih@desyneer.com>
2025-11-16 02:11:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDB = require('./config/database');
-const authRoutes = require('./routes/auth');
-const projectRoutes = require('./routes/projects');
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || '*',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-connectDB();
-
-app.use('/api/auth', authRoutes);
-app.use('/api/projects', projectRoutes);
 
 app.get('/api/health', (req, res) => {
   res.status(200).json({ message: 'Server is running' });
-});
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ message: 'Internal server error', error: err.message });
 });
 
 const PORT = process.env.PORT || 5000;
